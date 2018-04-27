63fa14424ab4c44edf8fea4c80e5af1c9aedb94f
liuhai <leon.liu@ubtrobot.com>
2018-04-27 11:00:50 +0800

    [需求]删除多余的so

--- actionmodule/src/main/obj/local/armeabi-v7a/objs/mp3lame/lame_3.99.5_libmp3lame/mpglib_interface.o.d
+++
@@ -1,2 +0,0 @@
-D:/Alpha1E_English/Alpha1Ebot_En/actionmodule/src/main/obj/local/armeabi-v7a/objs/mp3lame/lame_3.99.5_libmp3lame/mpglib_interface.o: \
-  D:/Alpha1E_English/Alpha1Ebot_En/actionmodule/src/main/jni/lame_3.99.5_libmp3lame/mpglib_interface.c
--- actionmodule/src/main/obj/local/x86_64/objs/mp3lame/lame_3.99.5_libmp3lame/mpglib_interface.o.d
+++
@@ -1,2 +0,0 @@
-D:/Alpha1E_English/Alpha1Ebot_En/actionmodule/src/main/obj/local/x86_64/objs/mp3lame/lame_3.99.5_libmp3lame/mpglib_interface.o: \
-  D:/Alpha1E_English/Alpha1Ebot_En/actionmodule/src/main/jni/lame_3.99.5_libmp3lame/mpglib_interface.c
