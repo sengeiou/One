cd45201cb2b181e83bc92849501d00dffd4f562c
“马伟民” <weimin.ma@ubtrobot.com>
2018-05-14 10:48:41 +0800

    [LoginModule][优化][V1.0.0.4]优化网络请求url

--- loginmodule/src/main/java/com/ubt/loginmodule/LoginHttpEntity.java
+++ loginmodule/src/main/java/com/ubt/loginmodule/LoginHttpEntity.java
@@ -24,18 +24,18 @@ public class LoginHttpEntity extends BaseHttpEntity{
     public static final String BASE_URL =BaseHttpEntity.BASIC_UBX_SYS /*"https://interface.ubtrobot.com/alpha1e/overseas/user/"*/; //正式环境
 //    public static final String BASE_URL = "http://10.10.1.14:8080/alpha1e/overseas/user/";  //测试环境
 //    public static final String BASE_LOGIN_URL = "https://test79.ubtrobot.com/user-service-rest/v2/"; //测试环境
-    public static final String BASE_LOGIN_URL = BaseHttpEntity.BASIC_THIRD_LOGIN_URL_EN; //正式环境
+    public static final String BASE_LOGIN_URL = BaseHttpEntity.BASE_UBX_COMMON; //正式环境
 
-    public static final String RESET = "user/password/reset";
-    public static final String LOGIN = "user/login";
-    public static final String LOGIN_THIRD = "user/login/third";
-    public static final String LOGIN_THIRD_FIRST = "user/login/third/isfirst";
-    public static final String VALIDATE_CODE = "user/captcha/verify-result";
+    public static final String RESET = "user-service-rest/v2/user/password/reset";
+    public static final String LOGIN = "user-service-rest/v2/user/login";
+    public static final String LOGIN_THIRD = "user-service-rest/v2/user/login/third";
+    public static final String LOGIN_THIRD_FIRST = "user-service-rest/v2/user/login/third/isfirst";
+    public static final String VALIDATE_CODE = "user-service-rest/v2/user/captcha/verify-result";
 //    public static final String FIND_PASSWORD = BASE_URL + "backPwd";
-    public static final String REGISTER = "user/register";
-    public static final String GET_CODE = "htmlEmail/ebotcaptcha";
-    public static final String USER_UPDATE = "overseas/user/update";
-    public static final String USER_GET_INFO = "overseas/user/get";
+    public static final String REGISTER = "user-service-rest/v2/user/register";
+    public static final String GET_CODE = "user-service-rest/v2/htmlEmail/ebotcaptcha";
+    public static final String USER_UPDATE = "alpha1e/overseas/user/update";
+    public static final String USER_GET_INFO = "alpha1e/overseas/user/get";
 
 
 
