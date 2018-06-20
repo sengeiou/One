ef8259299b7ea51037ce0a76ae75ff329541e5ff
“马伟民” <weimin.ma@ubtrobot.com>
2018-06-20 16:00:35 +0800

    Merge branch 'develop' of gitlab.ubt.com:liuhai/Alpha1Ebot_En into develop
    # Conflicts:
    #	blocklymodule/src/main/java/com/ubt/blockly/main/BlocklyActivity.java

--- blocklymodule/src/main/java/com/ubt/blockly/main/BlocklyActivity.java
+++ blocklymodule/src/main/java/com/ubt/blockly/main/BlocklyActivity.java
@@ -156,7 +156,7 @@ public class BlocklyActivity extends MVPBaseActivity<BlocklyContract.View, Block
 
             @Override
             public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-                    super.onReceivedSslError(view, handler, error);
+                super.onReceivedSslError(view, handler, error);
             }
 
             @Override
@@ -372,7 +372,7 @@ public class BlocklyActivity extends MVPBaseActivity<BlocklyContract.View, Block
 
     @Override
     public void noteRobotFallDown(int state) {
-        ViseLog.d("noteRobotFallDown:===" + state);
+        ViseLog.d("noteRobotFallDown:" + state);
         if(state == 3){
             callJavascript(true);
         }else{
